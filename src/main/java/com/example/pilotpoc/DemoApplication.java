e7a454469115d3ece36894b8fe03edb4d815b4af
Krunal Pala <krunal.apttus@gmail.com>
2018-01-30 20:33:51 +0530

    added URL

--- src/main/java/com/example/pilotpoc/DemoApplication.java
+++ src/main/java/com/example/pilotpoc/DemoApplication.java
@@ -1,12 +1,22 @@
 package com.example.pilotpoc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+	
+	@Value("${localDatabaseUrl}")
+    private static String localDatabaseUrl;
 
 	public static void main(String[] args) {
+		
+		logger.info("----localDatabaseUrl-----"+localDatabaseUrl);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
