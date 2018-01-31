a28385147b0c8c527267e40b3410da21b8a0c948
Krunal Pala <krunal.apttus@gmail.com>
2018-01-31 16:23:41 +0530

    refactoring

--- src/main/java/com/pilot/main/SecurityConfig.java
+++ src/main/java/com/pilot/main/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.pilot.main;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,16 +11,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeRequests()
-                	.antMatchers("*/fuelController/*","*/fuelController/customerPricing/").permitAll()
-        			.and().httpBasic();
+                	.and().httpBasic();
     }
     
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("admin").password("admin").roles("ADMIN")
-                .and().withUser("user").password("user").roles("USER");;
-    }
+   
 
 }
