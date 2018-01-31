031ad53842a8e46bf3871395b4c274e1efe9a8ef
Krunal Pala <krunal.apttus@gmail.com>
2018-01-31 15:19:58 +0530

    refactoring

--- src/main/java/com/pilot/controller/FuelContoller.java
+++ src/main/java/com/pilot/controller/FuelContoller.java
@@ -3,6 +3,7 @@ package com.pilot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class FuelContoller {
 	CustomerPricingRepository customerPricingRepository;
 	
 	// Get All Notes
-	@GetMapping("/customerPricing")
+	@GetMapping(path = "/customerPricing", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<CustomerPricingEntity> getAllNotes() {
 	    return (List<CustomerPricingEntity>) customerPricingRepository.findAll();
 	}
--- src/main/java/com/pilot/main/PilotMainSpringApp.java
+++ src/main/java/com/pilot/main/PilotMainSpringApp.java
@@ -2,21 +2,19 @@ package com.pilot.main;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class PilotMainSpringApp {
 	
 	private static final Logger logger = LoggerFactory.getLogger(PilotMainSpringApp.class);
 	
-	@Value("${localDatabaseUrl}")
-    private static String localDatabaseUrl;
-
 	public static void main(String[] args) {
-		
-		logger.info("----localDatabaseUrl-----"+localDatabaseUrl);
+		logger.info("starting spring");
 		SpringApplication.run(PilotMainSpringApp.class, args);
 	}
 }
