0ade325a8e8425d6955c41914418a3b9e047858d
Krunal Pala <krunal.apttus@gmail.com>
2018-01-31 16:46:09 +0530

    refactoring

--- src/main/java/com/pilot/controller/FuelContoller.java
+++ src/main/java/com/pilot/controller/FuelContoller.java
@@ -3,7 +3,6 @@ package com.pilot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,7 @@ public class FuelContoller {
 	CustomerPricingRepository customerPricingRepository;
 	
 	// Get All Notes
-	@GetMapping(path = "/customerPricing", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path = "/customerPricing")
 	public List<CustomerPricingEntity> getAllNotes() {
 	    return (List<CustomerPricingEntity>) customerPricingRepository.findAll();
 	}
