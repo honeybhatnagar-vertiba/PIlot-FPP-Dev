d25c3349a3a4392f91c6b567beb1c3d9efe12efd
Krunal Pala <krunal.apttus@gmail.com>
2018-01-31 15:57:57 +0530

    refactoring

--- src/main/java/com/pilot/main/SecurityConfig.java
+++ src/main/java/com/pilot/main/SecurityConfig.java
@@ -1,29 +1,31 @@
 package com.pilot.main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	// Authentication : User --> Roles
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
-		auth.inMemoryAuthentication().withUser("user1").password("secret1")
-				.roles("USER").and().withUser("admin1").password("secret1")
-				.roles("USER", "ADMIN");
-	}
 
-	// Authorization : Role -> Access
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().and().authorizeRequests().antMatchers("/**")
-				.hasRole("USER").antMatchers("/**").hasRole("ADMIN").and()
-				.csrf().disable().headers().frameOptions().disable();
-	}
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests().antMatchers("/","/fuelController/*","/fuelController/customerPricing/").permitAll()
+                .anyRequest().authenticated();
+                
+
+       httpSecurity.csrf().disable();
+       httpSecurity.headers().frameOptions().disable();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN")
+                .and().withUser("user").password("user").roles("USER");;
+    }
 
 }
