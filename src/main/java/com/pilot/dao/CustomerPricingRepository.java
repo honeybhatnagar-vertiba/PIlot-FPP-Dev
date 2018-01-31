49144e2d27687cdd9b91acf5c615b5ec3b9761ad
Krunal Pala <krunal.apttus@gmail.com>
2018-01-31 16:56:39 +0530

    refactoring

--- src/main/java/com/pilot/dao/CustomerPricingRepository.java
+++ src/main/java/com/pilot/dao/CustomerPricingRepository.java
@@ -1,9 +1,10 @@
 package com.pilot.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface  CustomerPricingRepository extends CrudRepository<CustomerPricingEntity, Long> {
 
 }
