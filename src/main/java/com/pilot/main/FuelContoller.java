24ed31f2615e84230d20bdc20573a67b22eb8c4b
Sai Pati <spati3@sapient.com>
2018-02-01 20:12:32 +0530

    first commit

--- src/main/java/com/pilot/main/FuelContoller.java
+++ src/main/java/com/pilot/main/FuelContoller.java
@@ -2,6 +2,8 @@ package com.pilot.main;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/fuelController")
 public class FuelContoller {
 	
+	private static final Logger logger = LoggerFactory.getLogger(PilotMainSpringApp.class);
+	
 	@Autowired
 	CustomerPricingRepository customerPricingRepository;
 	
 	// Get All Notes
 	@GetMapping(path = "/customerPricing")
 	public List<CustomerPricingEntity> getAllNotes() {
+		logger.info("---in Fuel Controller ---");
 	    return (List<CustomerPricingEntity>) customerPricingRepository.findAll();
 	}
 	
