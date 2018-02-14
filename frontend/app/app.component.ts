f8b0201f3bfe9e85f7f2f50ff4fc2c76e3fd3f17
Oswin Correa <oswinchris@gmail.com>
2018-02-14 13:23:17 +0530

    1

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -9,42 +9,55 @@ declare var tableau: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent{
-  title = 'app works!';
 
   users;
   tableauViz;
-  placeholderDiv;
-  url;
-  options;
-  
+
   constructor(private userService: UserService) {
 	this.users = userService.getUsers();
 	var that = this;
 	setTimeout(function(){
-		that.loadtableau('https://public.tableau.com/views/USregionalheatmap/Sheet2?:embed=y');
+		that.loadtableau('https://public.tableau.com/views/USMap_21/USMap?:embed=y');
 	}, 3000); 
   }
   
   loadtableau(tableauURL){
-	if(tableauURL != this.url){
-		
-		if(this.tableauViz != undefined){
-			this.tableauViz.dispose();
-		}	
-		
-		
-		this.placeholderDiv = document.getElementById('tableauViz');
-		this.url = tableauURL;
-		this.options = {
-		  hideTabs: true,
-		  width: '900px',
-		  height: '500px',
-		  onFirstInteractive: function() {
-			// The viz is now ready and can be safely used.
-		  }
-		};
-		this.tableauViz = new tableau.Viz(this.placeholderDiv, this.url, this.options);
-	}
+
+	if(this.tableauViz != undefined){
+		this.tableauViz.dispose();
+	}	
 	
+	var placeholderDiv = document.getElementById('tableauViz');
+	var options = {
+	  hideTabs: true,
+	  width: '900px',
+	  height: '500px',
+	  onFirstInteractive: function() {
+	  }
+	};
+	
+	this.tableauViz = new tableau.Viz(placeholderDiv, tableauURL, options);
+	
+	(<HTMLInputElement> document.getElementById("ytd_mtd_toggle")).checked = false;
+
+  }
+  
+  toggleYTDandMTD(){
+	  
+	  if(this.tableauViz != undefined){
+		  
+		  var activeSheet = this.tableauViz.getWorkbook().getActiveSheet();
+		  if((<HTMLInputElement> document.getElementById("ytd_mtd_toggle")).checked){
+			  
+			  activeSheet.applyFilterAsync(
+				"State",
+				"Tennessee",
+				tableau.FilterUpdateType.REPLACE);  
+		  }
+		  else{
+			  activeSheet.clearFilterAsync("State");
+		  }	
+	  }
+	  
   }
 }
