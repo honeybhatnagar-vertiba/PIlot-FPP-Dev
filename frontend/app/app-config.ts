9c5e982980471edfb2e41d53efdd4a5652fc2bb1
Honey Bhatnagar <honey.bhatnagar@vertiba.com>
2018-02-27 20:57:51 +0530

    change api url

--- frontend/app/app-config.ts
+++ frontend/app/app-config.ts
@@ -1,4 +1,5 @@
 export class AppConfig
 {
-    static PFJApiUrl:string="https://pilot-fpp-dev.herokuapp.com/";
+    //static PFJApiUrl:string="https://pilot-fpp-dev.herokuapp.com/";
+    static PFJApiUrl:string="https://fuel-pricing-platform.herokuapp.com/";
 }
